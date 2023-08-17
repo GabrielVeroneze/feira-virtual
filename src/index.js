5082cf7e20c55189bba361968727185f256fe20c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-17 15:51:18 -0300

    :sparkles: feat: Renderiza página de Login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { StyledEngineProvider, ThemeProvider, createTheme } from '@mui/material';
+import Login from './Pages/Login';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2A9F85'
+        },
+        secondary: {
+            main: '#FF7070'
+        },
+    }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <Login />
+            </ThemeProvider>
+        </StyledEngineProvider>
     </React.StrictMode>
 );
