ba91b78392a9966a6342752ee10733f4b3111ead
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-20 23:49:10 -0300

    :sparkles: feat: Armazena os valores de nome e saldo

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,13 +1,17 @@
 import { Button, Input, InputAdornment, InputLabel } from '@mui/material'
 import { Container, Titulo, InputContainer } from './styles'
 
-const Login = () => {
+const Login = ({ nome, setNome, saldo, setSaldo }) => {
     return (
         <Container>
             <Titulo>Insira o seu nome</Titulo>
             <InputContainer>
                 <InputLabel>Nome</InputLabel>
-                <Input type="text" />
+                <Input
+                    type="text"
+                    value={nome}
+                    onChange={event => setNome(event.target.value)}
+                />
             </InputContainer>
             <InputContainer>
                 <InputLabel>Saldo</InputLabel>
@@ -16,6 +20,8 @@ const Login = () => {
                     startAdornment={
                         <InputAdornment position="start">R$</InputAdornment>
                     }
+                    value={saldo}
+                    onChange={event => setSaldo(event.target.value)}
                 />
             </InputContainer>
             <Button variant="contained" color="primary">
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useState } from "react"
 import Carrinho from "Pages/Carrinho"
 import Feira from "Pages/Feira"
 import Login from "Pages/Login"
 
 const Router = () => {
+    const [nome, setNome] = useState('')
+    const [saldo, setSaldo] = useState(0)
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route
+                    path="/"
+                    element={
+                        <Login
+                            nome={nome}
+                            setNome={setNome}
+                            saldo={saldo}
+                            setSaldo={setSaldo}
+                        />
+                    }
+                />
                 <Route path="/feira" element={<Feira />} />
                 <Route path="/carrinho" element={<Carrinho />} />
             </Routes>
