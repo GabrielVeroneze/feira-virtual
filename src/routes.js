14d51b4a736c9dacac259288ce33277c4e44b5e0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-26 23:26:49 -0300

    :sparkles: feat: Fornece o contexto Usuario para as páginas de "login" e "feira"
    Cria rota de layout para renderizar o provedor de contexto e fornecer um valor de contexto para um conjunto de rotas.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
 import { UsuarioProvider } from "common/context/Usuario"
 import Carrinho from "Pages/Carrinho"
 import Feira from "Pages/Feira"
@@ -8,15 +8,16 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path="/"
+                <Route 
                     element={
                         <UsuarioProvider>
-                            <Login />
+                            <Outlet />
                         </UsuarioProvider>
-                    } 
-                />
-                <Route path="/feira" element={<Feira />} />
+                    }
+                >
+                    <Route path="/" element={<Login />} />
+                    <Route path="/feira" element={<Feira />} />
+                </Route>
                 <Route path="/carrinho" element={<Carrinho />} />
             </Routes>
         </BrowserRouter>
