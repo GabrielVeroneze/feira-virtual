efa638c4771bec82a033441596471ebdd9db1e42
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-26 23:36:15 -0300

    :sparkles: feat: Acessa o contexto de Usuario na página de feira

--- src/Pages/Feira/index.jsx
+++ src/Pages/Feira/index.jsx
@@ -2,15 +2,19 @@ import Produto from 'components/Produto'
 import NavBar from './NavBar'
 import feira from './feira.json'
 import { Container, Header, Lista } from './styles'
+import { useContext } from 'react'
+import { UsuarioContext } from 'common/context/Usuario'
 
 const Feira = () => {
+    const { nome, saldo } = useContext(UsuarioContext)
+
     return (
         <Container>
             <NavBar />
             <Header>
                 <div>
-                    <h2> Olá!</h2>
-                    <h3> Saldo: R$</h3>
+                    <h2> Olá {nome}!</h2>
+                    <h3> Saldo: R$ {saldo}</h3>
                 </div>
                 <p>Encontre os melhores produtos orgânicos!</p>
             </Header>
