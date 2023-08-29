79df3b227e763d46ed2d93f2ec25cabba4f6b4f7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-29 17:11:57 -0300

    :sparkles: feat: Adiciona na UI a quantidade de produtos

--- src/components/Produto/index.jsx
+++ src/components/Produto/index.jsx
@@ -1,35 +1,14 @@
-import { memo, useContext } from 'react'
+import { memo } from 'react'
 import { IconButton } from '@mui/material'
 import { Add, Remove } from '@mui/icons-material'
 import { Container } from './styles'
-import { CarrinhoContext } from 'common/context/Carrinho'
+import { useCarrinhoContext } from 'common/context/Carrinho'
 
 const Produto = ({ nome, foto, id, valor, unidade }) => {
-    const { carrinho, setCarrinho } = useContext(CarrinhoContext)
-    
-    const adicionarProduto = (novoProduto) => {
-        // Verifica se um novo produto com o mesmo ID já está no carrinho de compras. Se já estiver será definida como true, se não, será definida como false
-        const possuiProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id)
+    const { carrinho, adicionarProduto } = useCarrinhoContext()
 
-        // Essa parte é executada se o produto ainda não estiver no carrinho
-        if(!possuiProduto) {
-            novoProduto.quantidade = 1
-            setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto]) // Cria um novo array que inclui todos os itens existentes no carrinho mais o novo produto
-            return
-        }
+    const produtoNoCarrinho = carrinho.find(itemCarrinho => itemCarrinho.id === id)
 
-        // Essa parte é executada se o produto já existe o carrinho
-        setCarrinho(carrinhoAnterior => (
-            carrinhoAnterior.map(itemCarrinho => {
-                // Se isso for verdadeiro, significa que o produto que está sendo iterado é o mesmo que estamos tentando atualizar
-                if(itemCarrinho.id === novoProduto.id) {
-                    itemCarrinho.quantidade += 1 // A quantidade desse produto é incrementada em 1
-                }
-                return itemCarrinho
-            })
-        ))
-    }
-    
     return (
         <Container>
             <div>
@@ -42,6 +21,7 @@ const Produto = ({ nome, foto, id, valor, unidade }) => {
                 <IconButton color="secondary">
                     <Remove />
                 </IconButton>
+                {produtoNoCarrinho?.quantidade || 0}
                 <IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
                     <Add />
                 </IconButton>
