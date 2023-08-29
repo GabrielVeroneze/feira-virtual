4b338e66c54097eafeb2f4e95255038172eb6c46
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-29 17:11:04 -0300

    :recycle: refactor: Extraindo a função de adicionar produto
    Extrai a função de adicionar produto para dentro do hook customizado "useCarrinhoContext"

--- src/common/context/Carrinho.js
+++ src/common/context/Carrinho.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const CarrinhoContext = createContext()
 CarrinhoContext.displayName = "Carrinho"
@@ -11,4 +11,37 @@ export const CarrinhoProvider = ({ children }) => {
             {children}
         </CarrinhoContext.Provider>
     )
+}
+
+export const useCarrinhoContext = () => {
+    const { carrinho, setCarrinho } = useContext(CarrinhoContext)
+
+    const adicionarProduto = (novoProduto) => {
+        // Verifica se um novo produto com o mesmo ID já está no carrinho de compras. Se já estiver será definida como true, se não, será definida como false
+        const possuiProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id)
+
+        // Essa parte é executada se o produto ainda não estiver no carrinho
+        if(!possuiProduto) {
+            novoProduto.quantidade = 1
+            setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto]) // Cria um novo array que inclui todos os itens existentes no carrinho mais o novo produto
+            return
+        }
+
+        // Essa parte é executada se o produto já existe o carrinho
+        setCarrinho(carrinhoAnterior => (
+            carrinhoAnterior.map(itemCarrinho => {
+                // Se isso for verdadeiro, significa que o produto que está sendo iterado é o mesmo que estamos tentando atualizar
+                if(itemCarrinho.id === novoProduto.id) {
+                    itemCarrinho.quantidade += 1 // A quantidade desse produto é incrementada em 1
+                }
+                return itemCarrinho
+            })
+        ))
+    }
+
+    return {
+        carrinho,
+        setCarrinho,
+        adicionarProduto
+    }
 }
