c6fb4cb7f2648745d30b65f8a4a12ea24e211dad
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-08-28 22:49:08 -0300

    :bulb: docs: Adiciona comentários
    Adiciona comentários sobre a verificação da existência de um produto no carrinho e sobre as partes que serão executadas dependendo do resultado da verificação

--- src/components/Produto/index.jsx
+++ src/components/Produto/index.jsx
@@ -8,18 +8,22 @@ const Produto = ({ nome, foto, id, valor, unidade }) => {
     const { carrinho, setCarrinho } = useContext(CarrinhoContext)
     
     const adicionarProduto = (novoProduto) => {
+        // Verifica se um novo produto com o mesmo ID já está no carrinho de compras. Se já estiver será definida como true, se não, será definida como false
         const possuiProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id)
 
+        // Essa parte é executada se o produto ainda não estiver no carrinho
         if(!possuiProduto) {
             novoProduto.quantidade = 1
-            setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto])
+            setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto]) // Cria um novo array que inclui todos os itens existentes no carrinho mais o novo produto
             return
         }
 
+        // Essa parte é executada se o produto já existe o carrinho
         setCarrinho(carrinhoAnterior => (
             carrinhoAnterior.map(itemCarrinho => {
+                // Se isso for verdadeiro, significa que o produto que está sendo iterado é o mesmo que estamos tentando atualizar
                 if(itemCarrinho.id === novoProduto.id) {
-                    itemCarrinho.quantidade += 1
+                    itemCarrinho.quantidade += 1 // A quantidade desse produto é incrementada em 1
                 }
                 return itemCarrinho
             })
