f9e563178fef2a5e4c069b03bcd1a4eb5fd04bf0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-02 22:51:59 -0300

    :sparkles: feat: Exibe a quantidade total de produtos
    Utiliza do Hook Customizado "useCarrinhoContext" para exibir a quantidade total de produtos que existe no carrinho

--- src/Pages/Feira/NavBar/index.jsx
+++ src/Pages/Feira/NavBar/index.jsx
@@ -2,13 +2,19 @@ import { Badge, IconButton } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import { Nav } from "./styles"
+import { useCarrinhoContext } from "common/context/Carrinho";
 
 const NavBar = () => {
+    const { quantidadeProdutos } = useCarrinhoContext()
+
     return (
         <Nav>
             <Logo />
-            <IconButton>
-                <Badge color="primary">
+            <IconButton disabled={quantidadeProdutos === 0}>
+                <Badge 
+                    color="primary"
+                    badgeContent={quantidadeProdutos}
+                >
                     <ShoppingCart />
                 </Badge>
             </IconButton>
