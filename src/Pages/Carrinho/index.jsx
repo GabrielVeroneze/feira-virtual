3f0508c79999058b42be38457fa818723ac49a2d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-05 23:53:32 -0300

    :sparkles: feat: Utiliza o contexto de carrinho

--- src/Pages/Carrinho/index.jsx
+++ src/Pages/Carrinho/index.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import { Alert, Button, InputLabel, Snackbar } from '@mui/material'
 import { Container, PagamentoContainer, TotalContainer, Voltar } from './styles'
+import { useCarrinhoContext } from 'common/context/Carrinho'
+import Produto from 'components/Produto'
 
 const Carrinho = () => {
     const [openSnackbar, setOpenSnackbar] = useState(false)
+
+    const { carrinho } = useCarrinhoContext()
+    console.log(carrinho)
 
     return (
         <Container>
             <Voltar />
             <h2>Carrinho</h2>
+            {carrinho.map(produto => (
+                <Produto key={produto.id} {...produto} />
+            ))}
             <PagamentoContainer>
                 <InputLabel> Forma de Pagamento </InputLabel>
             </PagamentoContainer>
