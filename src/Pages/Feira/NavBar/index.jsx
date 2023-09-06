2c32e5ff1488fcab576ab18f1df7c8f868b797a1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-05 23:52:43 -0300

    :sparkles: feat: Utiliza o hook useNavigate para navegar
    Utiliza o hook useNavigate para navegar para a página de carrinho quando o ícone for clicado

--- src/Pages/Feira/NavBar/index.jsx
+++ src/Pages/Feira/NavBar/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Badge, IconButton } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
 import { ReactComponent as Logo } from 'assets/logo.svg';
@@ -6,11 +7,19 @@ import { useCarrinhoContext } from "common/context/Carrinho";
 
 const NavBar = () => {
     const { quantidadeProdutos } = useCarrinhoContext()
+    const navigate = useNavigate()
+
+    const redirecionar = () => {
+        navigate('/carrinho')
+    }
 
     return (
         <Nav>
             <Logo />
-            <IconButton disabled={quantidadeProdutos === 0}>
+            <IconButton
+                disabled={quantidadeProdutos === 0}
+                onClick={redirecionar}
+            >
                 <Badge 
                     color="primary"
                     badgeContent={quantidadeProdutos}
