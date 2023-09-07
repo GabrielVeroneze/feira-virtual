5c5857b119fe66276c25923543d24c031dd7dd72
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-07 17:51:16 -0300

    :sparkles: feat: Acessa o contexto de pagamento
    Acessa o contexto de pagamento na página de carrinho

--- src/Pages/Carrinho/index.jsx
+++ src/Pages/Carrinho/index.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Alert, Button, InputLabel, Snackbar } from '@mui/material'
 import { Container, PagamentoContainer, TotalContainer, Voltar } from './styles'
 import { useCarrinhoContext } from 'common/context/Carrinho'
 import Produto from 'components/Produto'
+import { PagamentoContext } from 'common/context/Pagamento'
 
 const Carrinho = () => {
     const [openSnackbar, setOpenSnackbar] = useState(false)
     const { carrinho } = useCarrinhoContext()
+    const { formaPagamento } = useContext(PagamentoContext)
     const navigate = useNavigate()
-
+    
     const redirecionar = () => {
         navigate(-1)
     }
 
+    console.log(formaPagamento);
+    
     return (
         <Container>
             <Voltar onClick={redirecionar} />
